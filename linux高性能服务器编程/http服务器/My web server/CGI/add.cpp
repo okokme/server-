aca4f71b9e34081d68763848aa865ecd4362efad
okokme <1216448825@qq.com>
2019-03-03 11:23:54 +0800

    完成POST请求

--- linux高性能服务器编程/http服务器/My web server/CGI/add.cpp
+++ linux高性能服务器编程/http服务器/My web server/CGI/add.cpp
@@ -1,29 +1,3 @@
-// #include <iostream>
-// #include <string>
-// #include <cstring>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <unistd.h>
-
-// using namespace std;
-
-// int main(int argc, char const *argv[])
-// {
-//     int a, b, accp_fd;
-//     sscanf( argv[0], "%d+%d,%d", &a, &b, &accp_fd);
-
-//     int t = a+b;
-//     string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\n\r\n";
-//     string body = "<html><head><title>okokme's CGI</title></head>";
-//     body += "<body><p>The result is " + to_string(a) + "+" + to_string(b) + "=" +to_string(t);
-//     body += </p></body></html>;
-//     response += body;
-//     dup2(accp_fd, STDOUT_FILENO);
-//     cout<< response.c_str()<<endl;
-    
-//     return 0;
-// }
-
 
 #include <iostream>
 #include <string>
@@ -39,13 +13,12 @@ int main( int argc, char **argv ) {
     sscanf( argv[0], "%d+%d,%d", &a, &b, &accp_fd );
 
     int t = a+b;
-    string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html;charset=utf-8\r\n\r\n";
+    string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html;\r\ncharset=utf-8\r\n\r\n";
     string body = "<html><head><title>okokme's CGI</title></head>";
     body += "<body><p>The result is " + to_string(a) + "+" + to_string(b) + " = " + to_string(t);
     body += "</p></body></html>";
     response += body;
     dup2(accp_fd, STDOUT_FILENO);
     cout << response.c_str();
-    // send( accp_fd, response.c_str(), response.size(), 0 );
     return 0;
 }
